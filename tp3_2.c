cbd4fb332e9f98ea50bab6263858e9234c9d5b54
Yazmin <moyanoyazminalejandra@gmail.com>
2024-04-16 12:20:50 -0300

    fin apartado b

--- tp3_2.c
+++ tp3_2.c
@@ -5,6 +5,7 @@
 #define M 12
 
 void cargarMatriz(float *p_matriz);
+void mostrarMatriz(float *p_matriz);
 
 int main(){
     srand(time(NULL));
@@ -13,16 +14,8 @@ int main(){
     puntero= &ganancias[0][0] ;
 
     cargarMatriz(puntero);
-
-    for (int i = 0; i < N; i++) 
-    {
-        for (int j = 0; j < M; j++)  
-        { 
-            printf("%2.f ", *(puntero + (i * M + j)));
-        }
-        printf("\n");
-        
-    }
+    mostrarMatriz(puntero);
+    
 
     return 0;
 }
@@ -35,6 +28,18 @@ void cargarMatriz(float *p_matriz){
         {
             *(p_matriz + (i * M + j)) = rand() %  40001 + 10000;  
         }
+        
+    }
+}
+
+void mostrarMatriz(float *p_matriz){
+    for (int i = 0; i < N; i++) 
+    {
+        for (int j = 0; j < M; j++)  
+        { 
+            printf("%2.f ", *(p_matriz + (i * M + j)));
+        }
+        printf("\n");
         
     }
 }
